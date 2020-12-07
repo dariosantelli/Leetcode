cbc1aba5908d6ffa6ce5adb468d47370ffa17184
Dario Santelli <dariosantelli@gmail.com>
2020-12-07 13:22:30 -0500

    Completed 70 recursive

---
+++ Dynamic Programming/70 Climbing Stairs/Recursive.py
@@ -0,0 +1,9 @@
+def climbStairs(self, n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    else:
+        return self.climbStairs(n - 1) + self.climbStairs(n - 2)
